d376baf20eb704f1680a81c46797bad3d0632728
oznka <ozankarslioglu@gmail.com>
2026-02-11 15:38:55 +0000

    Product page element finder

--- SeleniumWebTesting/SeleniumWebTests/C2_1_SortWebTablesTest.py
+++ SeleniumWebTesting/SeleniumWebTests/C2_1_SortWebTablesTest.py
@@ -17,26 +17,58 @@ from selenium import webdriver  # Import WebDriver for browser automation
 from selenium.webdriver.chrome.service import Service  # Import Service to set up the WebDriver with ChromeDriver
 from selenium.webdriver.common.by import By  # Import By to locate elements on the webpage
 import time  # Import time for adding delays in the script
+from selenium.webdriver.common.keys import Keys
 
 # Get the path to the ChromeDriver from environment variables
-chromedriver_path = os.getenv('CHROMEDRIVER_PATH')
+chromedriver_path = os.getenv("C:/Users/Ozan/.cache/selenium/chromedriver/win64/142.0.7444.175/chromedriver.exe")
 
 # Initialize the Chrome driver with the specified service (using the ChromeDriver path from the environment variable)
 service_obj = Service(chromedriver_path)
 driver = webdriver.Chrome(service=service_obj)
 
 # Open the specified URL in the browser (the page contains a list of vegetable offers)
-driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers")
+driver.get("https://www.waitrose.com")
 
 # Wait for 5 seconds to allow the page to load
-time.sleep(5)
+time.sleep(1)
 
-'''Pseudo code:
-Click on column header to sort the vegetable list
-Collect all item names --> browserSortedVeggieList (Assuming the browser does not sort properly)
-Sort the list manually and compare the two lists to check if sorting is working properly
-'''
+driver.find_element(By.XPATH, "//span[text()='Allow all']").click()
 
+time.sleep(1)
+search_input = driver.find_element(By.XPATH, "//input[@placeholder='Search groceries...']")
+search_input.click()
+search_input.send_keys('balsamic vinegar')
+search_input.send_keys(Keys.ENTER)
+
+time.sleep(1)
+search_input2 = driver.find_elements(By.XPATH, "//a[@data-origincomponent='ProductPod']")
+
+linksBalsamic = [item.get_attribute("href") for item in search_input2 if item.get_attribute("href")]
+
+for link in linksBalsamic[:3]:
+    print("\nlink:" + link)
+    driver.get(link)
+    time.sleep(1)
+    ingredientsDropdown = driver.find_element(By.XPATH, "//p[text()='Ingredients']")
+    ingredientsDropdown.click()
+    
+    element = driver.find_element(By.XPATH, "//div[contains(@class, 'Ingredient_ingredients')]")
+    print("element= " + element.text)
+
+    # Find by ID then find the text div inside it
+    # ingredients_text = driver.find_element(By.ID, "ingredients-region").text
+    # print("ingredients_text= " + ingredients_text)
+    # print(ingredients_text.get_attribute("textContent"))
+
+    """
+    ingredients = driver.find_element(By.XPATH, "//div[@aria-labelledby='ingredients-title']")
+    print("ingredients= " + ingredients.text)
+    """
+
+    driver.back()
+
+
+"""
 # Click on the column header ("Veg/fruit name") to trigger the sorting functionality
 driver.find_element(By.XPATH, "//span[text()='Veg/fruit name']").click()
 
@@ -66,6 +98,6 @@ assert browserSortedVeggieList == originalBrowserSortedList
 
 # If the assertion passes, print a success message
 print("The vegetable list is sorted correctly on the webpage.")
-
+"""
 # Close the browser after the test is completed
 driver.quit()
